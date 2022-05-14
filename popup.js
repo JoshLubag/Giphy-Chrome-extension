9ff1ff677a0985764fe93196e5d4a656f97d5c1c
Josh Lubag <josh.lubag@playngo.com>
2022-05-14 17:07:38 +0800

    Add GiphyAPI, first attempt at copy paste

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-const api_key = '' // enter your api key
+const api_key = 'wKTVuOaUTS48IVDju08FuEzZI1p6QIad' // enter your api key
 const search = document.querySelector("#search");
 search.addEventListener('keypress', (e) => {
     if (e.key === 'Enter') {
@@ -6,9 +6,12 @@ search.addEventListener('keypress', (e) => {
     }
 })
 function fetchImages(value) {
-    fetch(`http://api.giphy.com/v1/gifs/search?q=${value}&limit=9&api_key=${api_key}`)
+    fetch(`http://api.giphy.com/v1/gifs/translate?s=${value}&weirdness=9&api_key=${api_key}`)
         .then(res => res.json())
         .then(jsonData => {
+            console.log(JSON.stringify(jsonData.data.url));
+            console.log(JSON.stringify(jsonData.data.images.original.embed_url));
+           
             buildUI(jsonData)
         })
 }
@@ -17,16 +20,35 @@ function buildUI(response) {
     let column = document.querySelector('.columns')
     let len = response.data.length;
     column.innerHTML = '';
-    for (let index = 0; index < len; index++) {
-        let url = response.data[index].images.original.url;
+        let url = response.data.images.original.url;
         let img = document.createElement('img')
         img.setAttribute('src', url);
         column.appendChild(img)
-    }
 
 }
 
-document.body.addEventListener('click', (e) => {
+async function getImageBlobFromUrl(url) {
+    const fetchedImageData = await fetch(url)
+    const blob = await fetchedImageData.blob()
+    return blob
+  }
+  
+document.body.addEventListener('click', async (e) => {
     if (e.target.tagName === "IMG")
-        chrome.downloads.download({ url: e.target.src }, () => { })
+        {
+            try {
+                // console.log(e.target);
+                const blob = await getImageBlobFromUrl(e.target.src)
+                await navigator.clipboard.write([
+                  new ClipboardItem({
+                    [blob.type]: blob
+                  })
+                ])
+                alert(e.target.src)
+              } catch (err) {
+                console.error(err.name, err.message);
+                alert('There was an error while copying image to clipboard :/')
+              }
+        }
+       
 })
